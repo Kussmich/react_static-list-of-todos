97b555b821d1fa45c1f5750fe9dbb5bcf4ff5c5a
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-06-25 13:34:06 +0300

    Updates

---
+++ precache-manifest.b9ddc326321a07f1108ee786d0dcca60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fb69e6662c1aae06b4e160e1370ed6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45f2ce8ff53c14213654",
+    "url": "./static/css/main.e418b958.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f2ce8ff53c14213654",
+    "url": "./static/js/main.25227333.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
